66d730b369d7ba2fc724f01b46218d6f0cd5f8c0
Anton Medvedev <anton@medv.io>
2017-04-08 21:25:47 +0700

    Update npm task

--- docs/npm.md
+++ docs/npm.md
@@ -2,31 +2,20 @@
 
 ### Installing
 
-```php
-// deploy.php
-
-require 'vendor/deployer/recipes/npm.php';
-```
+~~~php
+require 'recipe/npm.php';
+~~~
 
 ### Configuration options
 
-- **bin/npm** *(optional)*: set NPM binary, automatically detected otherwise.
-- **local/bin/npm** *(optional)*: set local NPM binary, automatically detected otherwise. 
-
-By default, if no env setting is provided, this recipe will fallback to the global setting.
+- `bin/npm` *(optional)*: set NPM binary, automatically detected otherwise. 
 
 ### Tasks
 
 - `npm:install` Install NPM packages
-- `npm:local:install` Install NPM packages into a locally prepared release. This should be used with the [local recipe](docs/local.md).
 
 ### Suggested Usage
 
-
-```php
+~~~php
 after('deploy:update_code', 'npm:install');
-// or
-before('deploy:symlink', 'npm:install');
-// or local
-after('local:update_code', 'npm:local:install');
-```
+~~~
--- recipe/npm.php
+++ recipe/npm.php
@@ -13,28 +13,10 @@ set('bin/npm', function () {
 
 desc('Install npm packages');
 task('npm:install', function () {
-    $releases = get('releases_list');
-
-    if (isset($releases[1])) {
-        if (run("if [ -d {{deploy_path}}/releases/{$releases[1]}/node_modules ]; then echo 'true'; fi")->toBool()) {
-            run("cp --recursive {{deploy_path}}/releases/{$releases[1]}/node_modules {{release_path}}");
+    if (has('previous_release')) {
+        if (test('[ -d {{previous_release}}/node_modules ]')) {
+            run('cp -R {{previous_release}}/node_modules {{release_path}}');
         }
     }
     run("cd {{release_path}} && {{bin/npm}} install");
-});
-
-set('local/bin/npm', function () {
-  return (string)runLocally('which npm');
-});
-
-desc('Install npm packages');
-task('npm:local:install', function () {
-  $releases = get('local_releases_list');
-
-  if (isset($releases[1])) {
-    if (runLocally("if [ -d {{local_deploy_path}}/releases/{$releases[1]}/node_modules ]; then echo 'true'; fi")->toBool()) {
-      runLocally("cp -R {{local_deploy_path}}/releases/{$releases[1]}/node_modules {{local_release_path}}");
-    }
-  }
-  runLocally("cd {{local_release_path}} && {{local/bin/npm}} install");
 });
