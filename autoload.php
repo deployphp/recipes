d58d6277907a63e3bd319a972d3075ab75db6bd9
Anton Medvedev <anton@medv.io>
2017-04-10 20:50:23 +0700

    Add autoload.php

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+/* (c) Anton Medvedev <anton@medv.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
