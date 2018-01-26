c4fee788ec96746bfcc942b16e63856edd95d796
Dries Hooghe <dries.hooghe@gmail.com>
2018-01-26 15:49:24 +0100

    fix indentation

--- recipe/slack.php
+++ recipe/slack.php
@@ -10,9 +10,12 @@ namespace Deployer;
 use Deployer\Utility\Httpie;
 
 // Title of project
-set('slack_title', function () {
-	return get('application', 'Project');
-});
+set(
+	'slack_title',
+	function () {
+		return get('application', 'Project');
+	}
+);
 
 // Deploy message
 set('slack_text', '_{{user}}_ deploying `{{branch}}` to *{{target}}*');
@@ -37,86 +40,97 @@ set('slack_success_pretext', '');
 set('slack_failure_pretext', '');
 
 desc('Notifying Slack');
-task('slack:notify', function () {
-	if (!get('slack_webhook', false)) {
-		return;
+task(
+	'slack:notify',
+	function () {
+		if (!get('slack_webhook', false)) {
+			return;
+		}
+
+		$messageContent = [
+			'username' => get('slack_username'),
+			'icon_emoji' => get('slack_icon_emoji'),
+			'icon_url' => get('slack_icon_url'),
+			'attachments' => [
+				[
+					'title' => get('slack_title'),
+					'pretext' => get('slack_pretext'),
+					'text' => get('slack_text'),
+					'color' => get('slack_color'),
+					'mrkdwn_in' => ['text', 'pretext'],
+				],
+			],
+		];
+
+
+		Httpie::post(get('slack_webhook'))->body($messageContent)->send();
 	}
-
-	$messageContent = [
-		'username' => get('slack_username'),
-		'icon_emoji' => get('slack_icon_emoji'),
-		'icon_url' => get('slack_icon_url'),
-		'attachments' => [
-            [
-                'title' => get('slack_title'),
-                'pretext' => get('slack_pretext'),
-                'text' => get('slack_text'),
-                'color' => get('slack_color'),
-                'mrkdwn_in' => ['text', 'pretext'],
-		    ]
-        ],
-	];
-
-
-	Httpie::post(get('slack_webhook'))->body($messageContent)->send();
-})
+)
 	->once()
 	->shallow()
 	->setPrivate();
 
 desc('Notifying Slack about deploy finish');
-task('slack:notify:success', function () {
-	if (!get('slack_webhook', false)) {
-		return;
+task(
+	'slack:notify:success',
+	function () {
+		if (!get('slack_webhook', false)) {
+			return;
+		}
+
+		$messageContent = [
+			'username' => get('slack_username'),
+			'icon_emoji' => get('slack_icon_emoji'),
+			'icon_url' => get('slack_icon_url'),
+			'attachments' => [
+				[
+					'title' => get('slack_title'),
+					'pretext' => get('slack_success_pretext'),
+					'text' => get('slack_success_text'),
+					'color' => get('slack_success_color'),
+					'mrkdwn_in' => [
+						'text',
+						'pretext',
+					],
+				],
+			],
+		];
+
+
+		Httpie::post(get('slack_webhook'))->body($messageContent)->send();
 	}
-
-	$messageContent = [
-		'username' => get('slack_username'),
-		'icon_emoji' => get('slack_icon_emoji'),
-		'icon_url' => get('slack_icon_url'),
-		'attachments' => [
-                [
-                    'title' => get('slack_title'),
-                    'pretext' => get('slack_success_pretext'),
-                    'text' => get('slack_success_text'),
-                    'color' => get('slack_success_color'),
-                    'mrkdwn_in' => ['text', 'pretext'
-                ]
-            ],
-		],
-	];
-
-
-	Httpie::post(get('slack_webhook'))->body($messageContent)->send();
-})
+)
 	->once()
 	->shallow()
 	->setPrivate();
 
 desc('Notifying Slack about deploy failure');
-task('slack:notify:failure', function () {
-	if (!get('slack_webhook', false)) {
-		return;
+task(
+	'slack:notify:failure',
+	function () {
+		if (!get('slack_webhook', false)) {
+			return;
+		}
+
+		$messageContent = [
+			'username' => get('slack_username'),
+			'icon_emoji' => get('slack_icon_emoji'),
+			'icon_url' => get('slack_icon_url'),
+			'attachments' => [
+				[
+					'title' => get('slack_title'),
+					'pretext' => get('slack_failure_pretext'),
+					'text' => get('slack_failure_text'),
+					'color' => get('slack_failure_color'),
+					'mrkdwn_in' => ['text', 'pretext'],
+				],
+			],
+		];
+
+
+		Httpie::post(get('slack_webhook'))->body($messageContent)->send();
 	}
-
-	$messageContent = [
-		'username' => get('slack_username'),
-		'icon_emoji' => get('slack_icon_emoji'),
-		'icon_url' => get('slack_icon_url'),
-		'attachments' => [
-            [
-                'title' => get('slack_title'),
-                'pretext' => get('slack_failure_pretext'),
-                'text' => get('slack_failure_text'),
-                'color' => get('slack_failure_color'),
-                'mrkdwn_in' => ['text', 'pretext'],
-            ]
-        ],
-	];
-
-
-	Httpie::post(get('slack_webhook'))->body($messageContent)->send();
-})
+)
 	->once()
 	->shallow()
 	->setPrivate();
