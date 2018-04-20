d889eb4b00b3facd90229a477d1e5f01c0b08260
Lucas Mezêncio <lucas.mezencio@gmail.com>
2018-04-20 02:56:59 -0300

    Add Discord recipe (#177)
    * Add psr-4 autoload
    
    * Add default messaging
    
    * Add Discord recipe
    
    * Update docs
    
    * Remove psr-4 autoloading
    
    * Attach Discord Messaging class into Discord recipe
    
    * Remove classes and namespaces
    
    * Update readme

---
+++ recipe/discord.php
@@ -0,0 +1,67 @@
+<?php
+/* (c) Lucas Mezêncio <lucas.mezencio@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Deployer\Task\Context;
+use Deployer\Utility\Httpie;
+
+set('discord_webhook', function () {
+    return 'https://discordapp.com/api/webhooks/{{discord_channel}}/{{discord_token}}/slack';
+});
+
+// Deploy messages
+set('discord_notify_text', [
+    'text' => ':information_source: **{{user}}** is deploying branch `{{branch}}` to _{{target}}_',
+]);
+set('discord_success_text', [
+    'text' => ':white_check_mark: Branch `{{branch}}` deployed to _{{target}}_ successfully',
+]);
+set('discord_failure_text', [
+    'text' => ':no_entry_sign: Branch `{{branch}}` has failed to deploy to _{{target}}_',
+]);
+
+// Helpers
+set('send_message', function ($data) {
+    Httpie::post(get('discord_webhook'))->body($data)->send();
+});
+
+// Tasks
+desc('Just notify your Discord channel with all messages, without deploying');
+task('discord:test', function () {
+    $notify = get('discord_notify_text');
+    $success = get('discord_success_text');
+    $failure = get('discord_failure_text');
+
+    get('send_message')($notify);
+    get('send_message')($success);
+    get('send_message')($failure);
+})
+    ->once()
+    ->shallow();
+
+desc('Notify Discord');
+task('discord:notify', function () {
+    get('send_message')(get('discord_notify_text'));
+})
+    ->once()
+    ->shallow()
+    ->isPrivate();
+
+desc('Notify Discord about deploy finish');
+task('discord:notify:success', function () {
+    get('send_message')(get('discord_success_text'));
+})
+    ->once()
+    ->shallow()
+    ->isPrivate();
+
+desc('Notify Discord about deploy failure');
+task('discord:notify:failure', function () {
+    get('send_message')(get('discord_failure_text'));
+})
+    ->once()
+    ->shallow()
+    ->isPrivate();
